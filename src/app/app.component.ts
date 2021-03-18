46503c802189a6f18fb9cd88ba27f47d7b91819c
Michelle Wong <michellekwong2@gmail.com>
2021-03-18 18:18:30 -0400

    Installed bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ProductService} from 'src/app/product-service/product.service'
+import { Product } from './product/product';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Services And DI';
+  products!: Product[];
+  productService: any;
+
+  constructor(){
+    //creating variable for new instance of Product Service
+    this.productService = new ProductService;
+  }
+  getProductList(){
+    //setting variable products, from what's returned from the instance of ProductService's function-> getProducts
+    this.products = this.productService.getProducts();
+  }
 }
